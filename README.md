285cd51577b199ab75aaaef5b2f62839dffbcb6c
Jeanie Chea <chea.jeanie@gmail.com>
2022-04-04 02:46:37 -0700

    Finish unit tests

--- README.md
+++ README.md
@@ -3,11 +3,43 @@
 ## Approach
 - Split problem into steps, split steps into smaller steps, repeat
 - Work on the data cleaning of the two csv files first then work on the api
+- Lots of research
 - Data cleaning:
+  - Experiment with the data in a jupyter notebook to vizualize dataframes
   - Read the csv files into two separate dataframes
-  - Play around with loc to filter out data
-
+  - Filter and clean data
+  - Organize into functions
+  - Write Unit tests for the functions
 - Api:
+  - Use Docker config files to get Docker running for the environment variables
+  - Use Postman for the requests
+  - Write unit tests for the api routes
+
+## Learned
+- More about Pandas, apis, and unit testing
+- Regex
+- Practice more on splitting the problem into smaller steps
+- Problem solving
+- How to Research a lot
+- Git: Work on a separate branch then make a pull request. Trying not to commit to main
 
 ## Challenges Encountered
 - Reading csv into dataframes with a | delimiter (solved)
+- Using regex to filter out the package (solved)
+- Could not get Docker-compose to work on MacOS machine, dependency issues (solved by using a Windows machine)
+  - Could not get any of the environment variables without running Docker-compose on MacOS
+  - Kept getting jinja2 errors in my python3.7 folder which does not exist in my MacOS machine
+- Having trouble passing Unit tests for the products and update_merchant routes as I've been getting a 400 code
+  - 3/5 test cases passed though!
+- Could not figure out how to automatically inject data straight into the api as a POST request
+  - However, I was able to successfully make a POST request with the required body manually
+  - Maybe format data into json to match with the json_schema then loop through each row of data while making one POST request at a time (?)
+
+## Future Improvements
+- Figure out how to pass the unit tests for the products and update_merchant routes
+- Automate injecting data into the api
+
+## Final Thoughts
+- I had a lot of fun working on this take home test!
+- I am willing to learn more and improve my skills
+- Feedback is always appreciated
--- integration-skill-test-server-master/webapp/tests.py
+++ integration-skill-test-server-master/webapp/tests.py
@@ -1,6 +1,13 @@
 import unittest
-
+import json
 from app import app
+import os
+
+GRAND_TYPE = ["client_credentials", "refresh_token"]
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+RICHARD_ID = os.environ.get("RICHARD_ID")
+BEAUTY_ID = os.environ.get("BEAUTY_ID")
 
 
 class AppTestCase(unittest.TestCase):
@@ -8,19 +15,80 @@ class AppTestCase(unittest.TestCase):
     def test_ping(self):
         tester = app.test_client(self)
         response = tester.get('/ping')
-        assert 'PONG' in response.data
-        
+        assert b'PONG' in response.data
+
     def test_get_token(self):
-        pass
-    
+        mock_params = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': GRAND_TYPE
+        }
+
+        tester = app.test_client(self)
+        response = tester.post('/oauth/token', query_string=mock_params)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
     def test_product(self):
-        pass
-    
+        mock_request_headers = {
+            'token': 'Bearer JcJTkYYkJJYYhYTcAjkZ',
+            'Content-Type': 'application/json'
+        }
+
+        mock_request_data = {
+            "merchant_id": RICHARD_ID,
+            "sku": "251261",
+            "barcodes": ["72649244"],
+            "brand": "Lubritek",
+            "name": "Toallita Humeda C/30 Pzas",
+            "description": "Toallita Humeda C30 Pzas 1 Pza",
+            "package": "1 PZA",
+            "image_url": "https://locahost:8000/images/72649244?height=500&width=500",
+            "category": "Hardlines | Sporting Goods | Piscina & Juegos Agua",
+            "url": None,
+            "branch_products": [{
+                "branch": "MM",
+                "stock": 4,
+                "price": 90.3
+            }
+            ]
+        }
+
+        tester = app.test_client(self)
+        response = tester.post(
+            '/api/products', json=json.dumps(mock_request_data), headers=mock_request_headers)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
     def test_merchants(self):
-        pass
-    
+        mock_request_headers = {
+            'token': 'Bearer JcJTkYYkJJYYhYTcAjkZ'
+        }
+        tester = app.test_client(self)
+        response = tester.get('/api/merchants', headers=mock_request_headers)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
     def test_update_merchant(self):
-        pass
+        mock_request_headers = {
+            'token': 'Bearer JcJTkYYkJJYYhYTcAjkZ',
+            'Accept': '*/*'
+        }
+
+        mock_request_data = {
+            "id": "ae9c81fe-163e-4546-8349-19dbf63715c7",
+            "name": "Richard's",
+            "is_active": True,
+            "can_be_updated": True,
+            "can_be_deleted": False
+        }
+
+        tester = app.test_client(self)
+        response = tester.put('/api/merchants/ae9c81fe-163e-4546-8349-19dbf63715c7',
+                              json=json.dumps(mock_request_data), headers=mock_request_headers, content_type='application/json')
+
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
 
 
 if __name__ == '__main__':
